c8fd3f5eaca27e7f2d3b4d6a170c28a81c7ea517
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-04 17:07:59 +0200

    "prismaclient"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,2 +1,3 @@
-import NextAuth from 'next-auth/next'; import CredentialsProvider from 'next-auth/providers/credentials'; import bcrypt from 'bcrypt';
-export const authOptions = {}
+import NextAuth from 'next-auth/next'; import CredentialsProvider from 'next-auth/providers/credentials'; import bcrypt from 'bcrypt'; 
+import { PrismaAdapter } from '@auth/prisma-adapter'; import { PrismaClient } from '@prisma/client';
+const prisma = new PrismaClient(); export const authOptions = { adapter: PrismaAdapter(prisma) }
