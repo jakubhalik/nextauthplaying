199647911da246fd6b852cbd6729b5cff7de843e
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-04 18:02:19 +0200

    "somenewstuff"

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,2 +1,4 @@
-'use client'; import './globals.css';
-export default function App() { return <></>; }
+'use client'; import './globals.css'; import Link from 'next/link';
+export default function App() { return <main>
+    <h1>auth</h1><Link href="/register">Register Page</Link><Link href="/login">Login Page</Link>
+</main>; }
