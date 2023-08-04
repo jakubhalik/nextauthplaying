8091ca9842fcaa9a5beca0e43e1c0060abc0637f
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-04 16:49:11 +0200

    "desperateeffort"

---
+++ src/app/api/auth/[...nextauth]/options.ts
@@ -0,0 +1,2 @@
+import type { NextAuthOptions } from 'next-auth';
+export const options: NextAuthOptions = { providers: [] }
---
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -0,0 +1,2 @@
+import NextAuth from 'next-auth';
+const handler = NextAuth(); export { handler as GET, handler as POST };
