e9757fb0a15a3f12f71cb2156325f50133d64836
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-08 22:41:52 +0200

    "tinyfixes"

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -14,9 +14,9 @@ type AuthFormProps = {
 export const AuthForm = ({ title, buttonText, data, handleChange, handleSubmit }: AuthFormProps) => <div className="flex justify-center py-24 3xl:py-56">
     <div className="border rounded-md p-4 sm:p-14">
         <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
-        <form className="space-y-6" onSubmit={handleSubmit}>
+        <form onSubmit={handleSubmit}>
             <Input name="name" type="text" value={data.name} onChange={handleChange} />
-            <Input name="email" type="email" value={data.email} onChange={handleChange} />
+            <Input name="email address" type="email" value={data.email} onChange={handleChange} />
             <Input name="password" type="password" value={data.password} onChange={handleChange} />
             <div className="mt-4">
                 <button type="submit" className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm 
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -3,8 +3,7 @@ import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '
 export default function RegisterPage() { 
     const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter();
     const registerUser = async (e: FormEvent) => {
-        e.preventDefault(); 
-        const response = await fetch('/api/register', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
+        e.preventDefault(); const response = await fetch('/api/register', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
         await response.json(); router.push('/login');
     }
     return <AuthForm  title="Register an account" buttonText="Register" data={data} handleChange={handleChange} handleSubmit={registerUser} />;
