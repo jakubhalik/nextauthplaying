9536a4f66345a78cc307d027a2f03eefe765dd8e
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 13:36:14 +0200

    "brevitybutbroken"

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -1,29 +1,22 @@
 import { ChangeEvent, FormEvent } from 'react';
-type InputProps = { name: string, type: 'text' | 'email' | 'password', value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void }
-const Input: React.FC<InputProps> = ({ name, type, value, onChange }) => <>
-    <label htmlFor={name} className={`block text-lg font-medium text-gray-900 ${name !== 'name' ? "mt-3 sm:mt-4" : ""}`}>
-        {name.charAt(0).toUpperCase() + name.slice(1)}
-    </label>
-    <input id={name} name={name} type={type} required value={value} className="mt-2 block w-full rounded-md p-2 border-0 text-gray-900 shadow-sm ring-1 ring-inset 
-    ring-gray-300 placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md" onChange={onChange} />
+const Input = ({ name, value, onChange }: { name: string, value: string, onChange: (e: ChangeEvent<HTMLInputElement>) => void}) => <>
+    <label htmlFor={name} className={`block text-lg font-medium text-gray-900 ${name !== 'name' ? "mt-3 sm:mt-4" : ""}`}>{name.charAt(0).toUpperCase() + name.slice(1)}</label>
+    <input id={name} type={name} required value={value} onChange={onChange} className="mt-2 block w-full rounded-md p-2 border text-gray-900 shadow-sm ring-1 ring-inset ring-gray-300 
+    placeholder:text-gray-400 focus:ring-2 focus:ring-inset focus:ring-indigo-600 sm:text-md" />
 </>;
-type AuthFormProps = { 
-    title: string, buttonText: string, data: { name: string, email: string, password: string }, 
-    handleChange: (e: ChangeEvent<HTMLInputElement>) => void, handleSubmit: (e: FormEvent<HTMLFormElement>) => void 
-}
-export const AuthForm = ({ title, buttonText, data, handleChange, handleSubmit }: AuthFormProps) => <div className="flex justify-center py-24 3xl:py-56">
-    <div className="border rounded-md p-4 sm:p-14">
-        <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
-        <form onSubmit={handleSubmit}>
-            <Input name="name" type="text" value={data.name} onChange={handleChange} />
-            <Input name="email" type="email" value={data.email} onChange={handleChange} />
-            <Input name="password" type="password" value={data.password} onChange={handleChange} />
-            <div className="mt-4">
-                <button type="submit" className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm 
-                hover:bg-sky-400 active:bg-sky-300">
-                    {buttonText}
-                </button>
+export const AuthForm = ({ title, buttonText, data, handleChange, handleSubmit }: 
+    { title: string, buttonText: string, data: { [key: string]: string }, handleChange: (e: ChangeEvent<HTMLInputElement>) => void, handleSubmit: (e: FormEvent<HTMLFormElement>) => void }) => 
+        <div className="flex justify-center py-24 3xl:py-56">
+            <div className="border rounded-md p-4 sm:p-14">
+                <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
+                <form onSubmit={handleSubmit}>
+                    {Object.keys(data).map(key => <Input key={key} name={key} value={data[key]} onChange={handleChange} />)}
+                    <div className="mt-4">
+                        <button type="submit" className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm 
+                        hover:bg-sky-400 active:bg-sky-300">
+                            {buttonText}
+                        </button>
+                    </div>
+                </form>
             </div>
-        </form>
-    </div>
-</div>;
+        </div>;
