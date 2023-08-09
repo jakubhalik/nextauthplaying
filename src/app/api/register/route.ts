5ba411e9a5a8b725d8d7d7a447d115ca96f05b67
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 17:45:41 +0200

    "routedownto5linesbrevity"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,5 @@
 import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
 const prisma = new PrismaClient();
-export async function POST(request: NextRequest) { 
-    const { name, email, password } = await request.json(); 
-    if(!(name && email && password) || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
-    return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
-}
+export const POST = async (request: { json: () => any; }) => (!(await request.json()).email || await prisma.user.findUnique({ where: { email: (await request.json()).email } })) ? 
+    new NextResponse('Missing name, email, or password', { status: 400 }) 
+    : NextResponse.json(await prisma.user.create({ data: { ...(await request.json()), hashedPassword: await bcrypt.hash((await request.json()).password, 10) } }));
