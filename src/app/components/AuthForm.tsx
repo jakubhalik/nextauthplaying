760106101dbf8fcecae2f15b64bb42bba3052c7a
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 13:50:19 +0200

    "tinybetternotimportant"

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -6,17 +6,15 @@ const Input = ({ name, value, onChange }: { name: string, value: string, onChang
 </>;
 export const AuthForm = ({ title, buttonText, data, handleChange, handleSubmit }: 
     { title: string, buttonText: string, data: { [key: string]: string }, handleChange: (e: ChangeEvent<HTMLInputElement>) => void, handleSubmit: (e: FormEvent<HTMLFormElement>) => void }) => 
-        <div className="flex justify-center py-24 3xl:py-56">
-            <div className="border rounded-md p-4 sm:p-14">
-                <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
-                <form onSubmit={handleSubmit}>
-                    {Object.keys(data).map(key => <Input key={key} name={key} value={data[key]} onChange={handleChange} />)}
-                    <div className="mt-4">
-                        <button type="submit" className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm 
-                        hover:bg-sky-400 active:bg-sky-300">
-                            {buttonText}
-                        </button>
-                    </div>
-                </form>
-            </div>
-        </div>;
+        <div className="flex justify-center py-24 3xl:py-56"><div className="border rounded-md p-4 sm:p-14">
+            <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
+            <form onSubmit={handleSubmit}>
+                {Object.keys(data).map(key => <Input key={key} name={key} value={data[key]} onChange={handleChange} />)}
+                <div className="mt-4">
+                    <button type="submit" 
+                    className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm hover:bg-sky-400 active:bg-sky-300">
+                        {buttonText}
+                    </button>
+                </div>
+            </form>
+        </div></div>;
