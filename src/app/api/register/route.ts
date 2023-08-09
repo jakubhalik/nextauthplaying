9f42c814e7468f47e43fbade87aa4593997c1235
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 17:06:44 +0200

    "betterroutebrevity"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,7 @@
 import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) { 
-    const { name, email, password } = await request.json(); if(!name || !email || !password) return new NextResponse('Missing name, email, or password', { status: 400 });
-    if (await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('User already exists', { status: 400 });
+    const { name, email, password } = await request.json(); 
+    if(!name || !email || !password || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
     return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
 }
