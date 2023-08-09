53808839d85a92d356c8cab170901d95c764905f
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 17:10:41 +0200

    "nicerroutecode"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -2,6 +2,6 @@ import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; impo
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) { 
     const { name, email, password } = await request.json(); 
-    if(!name || !email || !password || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
+    if(!(name && email && password) || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
     return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
 }
