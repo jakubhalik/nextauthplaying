1ea1acf1f4ba07a05d00c758ef9ba7e1fd8dbc7f
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 17:54:04 +0200

    "typedroute"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,5 @@
 import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
 const prisma = new PrismaClient();
-export const POST = async (request: { json: () => any; }) => (!(await request.json()).email || await prisma.user.findUnique({ where: { email: (await request.json()).email } })) ? 
+export const POST = async (request: NextRequest) => (!(await request.json()).email || await prisma.user.findUnique({ where: { email: (await request.json()).email } })) ? 
     new NextResponse('Missing name, email, or password', { status: 400 }) 
     : NextResponse.json(await prisma.user.create({ data: { ...(await request.json()), hashedPassword: await bcrypt.hash((await request.json()).password, 10) } }));
