6fcb4649e881cbd0c77a95ca9b76934c0b19e251
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-09 16:36:36 +0200

    "loginbrevity"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,9 +1,8 @@
-'use client'; import { useRouter } from 'next/navigation'; import { FormEvent } from 'react'; import { signIn } from 'next-auth/react';
+'use client'; import { useRouter } from 'next/navigation'; import { signIn } from 'next-auth/react';
 import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
-export default function loginPage() { 
-    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter();
-    const loginUser = async (e: FormEvent) => {
-        e.preventDefault(); const result = await signIn('credentials', { ...data, redirect: false }); if (!result?.error) router.push('/dashboard');
-    }
-    return <AuthForm  title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} handleSubmit={loginUser} />;
+export default function LoginPage() { 
+    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter(); return <AuthForm 
+        title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} 
+        handleSubmit={e => {e.preventDefault(); signIn('credentials', { ...data, redirect: false }).then(result => { if (!result?.error) router.push('/dashboard') })}}
+    />;
 }
--- src/app/register/page.tsx
+++ src/app/register/page.tsx
@@ -1,10 +1,7 @@
-'use client'; import { useRouter } from 'next/navigation'; import { FormEvent } from 'react'; 
-import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
+'use client'; import { useRouter } from 'next/navigation'; import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
 export default function RegisterPage() { 
-    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter();
-    const registerUser = async (e: FormEvent) => {
-        e.preventDefault(); const response = await fetch('/api/register', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) });
-        await response.json(); router.push('/login');
-    }
-    return <AuthForm  title="Register an account" buttonText="Register" data={data} handleChange={handleChange} handleSubmit={registerUser} />;
+    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter(); return <AuthForm  
+        title="Register an account" buttonText="Register" data={data} handleChange={handleChange} handleSubmit=
+            {async e => {e.preventDefault(); await fetch('/api/register', { method: 'POST', headers: { 'Content-Type': 'application/json' }, body: JSON.stringify(data) }); router.push('/login');}} 
+    />;
 }
