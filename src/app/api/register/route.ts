303b775535487ad976ac095b4875261dc35d8213
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-10 12:59:42 +0200

    "cantregisterwithalreadyusednameorpasswordorboth"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,7 +1,9 @@
 import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) { 
-    const { name, email, password } = await request.json(); 
-    if(!(name && email && password) || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
+    const requestData = await request.json(); const { name, email, password } = requestData;
+    const existingEmail = await prisma.user.findFirst({ where: { email } }); const existingName = await prisma.user.findFirst({ where: { name } });
+    if (existingEmail) return new NextResponse('Email already exists', { status: 400 }); if (existingName) return new NextResponse('Name already exists', { status: 400 });
+    if (!name || !email || !password) return new NextResponse('Missing name, email, or password', { status: 400 });
     return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
 }
