64385a705a7716c130ffd79ffacd5342260e7750
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-10 14:13:11 +0200

    "brevity"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,9 +1,9 @@
-import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
+import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest, NextResponse } from 'next/server';
 const prisma = new PrismaClient();
-export async function POST(request: NextRequest) { 
-    const requestData = await request.json(); const { name, email, password } = requestData;
-    const existingEmail = await prisma.user.findFirst({ where: { email } }); const existingName = await prisma.user.findFirst({ where: { name } });
-    if (existingEmail) return new NextResponse('Email already exists', { status: 400 }); if (existingName) return new NextResponse('Name already exists', { status: 400 });
+export async function POST(request: NextRequest) {
+    const { name, email, password } = await request.json();
     if (!name || !email || !password) return new NextResponse('Missing name, email, or password', { status: 400 });
-    return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
+    if (await prisma.user.findFirst({ where: { email } })) return new NextResponse('Email already exists', { status: 400 });
+    if (await prisma.user.findFirst({ where: { name } })) return new NextResponse('Name already exists', { status: 400 });
+    return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } }));
 }
