335027960a1e7ab3647bd31d3ad7c61ae5af89fd
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-10 14:54:23 +0200

    "betterrouteconditionality"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -3,7 +3,7 @@ const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
     const { name, email, password } = await request.json();
     if (!name || !email || !password) return new NextResponse('Missing name, email, or password', { status: 400 });
-    if (await prisma.user.findFirst({ where: { email } })) return new NextResponse('Email already exists', { status: 400 });
-    if (await prisma.user.findFirst({ where: { name } })) return new NextResponse('Name already exists', { status: 400 });
+    const existingUser = await prisma.user.findFirst({ where: { OR: [ { name }, { email } ] } });
+    if (existingUser) return new NextResponse(`${existingUser.email === email ? 'Email' : 'Name'} already exists`, { status: 400 });
     return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } }));
 }
