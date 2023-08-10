b04b138c05e0a38be8835d86eec9ff90e15463a9
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-10 12:16:54 +0200

    "fix"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -2,9 +2,9 @@ import NextAuth from 'next-auth/next'; import CredentialsProvider from 'next-aut
 import { PrismaAdapter } from '@auth/prisma-adapter'; import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const authorize = async ({ name, email, password }: any) => { 
-    if(!name || !email || !password) return null; const user: any = await prisma.user.findUnique({ where: { email } }); 
+    if(!(name || email || password)) return null; const user: any = await prisma.user.findUnique({ where: { email } }); 
     return user && await bcrypt.compare(password, user.hashedPassword) ? user : null;
-}; 
+};
 const authOptions: { adapter: any, providers: any[], session: { strategy: 'jwt' }, secret: string, debug: boolean } = {
     adapter: PrismaAdapter(prisma), providers: [ CredentialsProvider({ 
         name: 'credentials', credentials: { username: { label: 'Username', type: 'text', placeholder: 'jsmith' }, password: { label: 'Password', type: 'password' } }, authorize 
--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,5 +1,7 @@
 import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest ,NextResponse } from 'next/server';
 const prisma = new PrismaClient();
-export const POST = async (request: NextRequest) => (!(await request.json()).email || await prisma.user.findUnique({ where: { email: (await request.json()).email } })) ? 
-    new NextResponse('Missing name, email, or password', { status: 400 }) 
-    : NextResponse.json(await prisma.user.create({ data: { ...(await request.json()), hashedPassword: await bcrypt.hash((await request.json()).password, 10) } }));
+export async function POST(request: NextRequest) { 
+    const { name, email, password } = await request.json(); 
+    if(!(name && email && password) || await prisma.user.findUnique({ where: { email: email } })) return new NextResponse('Missing name, email, or password', { status: 400 });
+    return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } })); 
+}
