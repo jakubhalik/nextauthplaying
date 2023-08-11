ef6d689de949a86b235a8d91a16f41e3e89729d3
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-11 14:13:44 +0200

    "dynamicerrorcomponent"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,5 +1,5 @@
-'use client'; import { useRouter } from 'next/navigation'; import { signIn } from 'next-auth/react';
-import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
+'use client'; 
+import { useRouter } from 'next/navigation'; import { signIn } from 'next-auth/react'; import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
 export default function LoginPage() { 
     const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter(); return <AuthForm 
         title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} 
