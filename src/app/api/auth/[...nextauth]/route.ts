4193c5f2f6a31871201dee3c9b766448ed485d60
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-11 16:21:49 +0200

    "errorpopupinloginpage"

--- src/app/api/auth/[...nextauth]/route.ts
+++ src/app/api/auth/[...nextauth]/route.ts
@@ -1,13 +1,16 @@
-import NextAuth from 'next-auth/next'; import CredentialsProvider from 'next-auth/providers/credentials'; import bcrypt from 'bcrypt'; 
+import NextAuth from 'next-auth/next'; import CredentialsProvider from 'next-auth/providers/credentials'; import bcrypt from 'bcrypt';
 import { PrismaAdapter } from '@auth/prisma-adapter'; import { PrismaClient } from '@prisma/client';
 const prisma = new PrismaClient();
 const authorize = async ({ name, email, password }: any) => { 
-    if(!(name || email || password)) return null; const user: any = await prisma.user.findUnique({ where: { email } }); 
-    return user && await bcrypt.compare(password, user.hashedPassword) ? user : null;
+    if(!(name || email || password)) throw new Error("Incomplete credentials"); 
+    const user: any = await prisma.user.findUnique({ where: { email } }); if (!user) throw new Error("Account does not exist");
+    const isValidPassword = await bcrypt.compare(password, user.hashedPassword); if (!isValidPassword) throw new Error("Wrong password");
+    return user;
 };
 const authOptions: { adapter: any, providers: any[], session: { strategy: 'jwt' }, secret: string, debug: boolean } = {
-    adapter: PrismaAdapter(prisma), providers: [ CredentialsProvider({ 
-        name: 'credentials', credentials: { username: { label: 'Username', type: 'text', placeholder: 'jsmith' }, password: { label: 'Password', type: 'password' } }, authorize 
-    })], session: { strategy: 'jwt' }, secret: process.env.NEXTAUTH_SECRET!, debug: process.env.NODE_ENV === 'development'
+    adapter: PrismaAdapter(prisma),
+    providers: [ CredentialsProvider({
+            name: 'credentials', credentials: { username: { label: 'Username', type: 'text', placeholder: 'jsmith' }, password: { label: 'Password', type: 'password' } }, authorize 
+        }) ], session: { strategy: 'jwt' }, secret: process.env.NEXTAUTH_SECRET!, debug: process.env.NODE_ENV === 'development'
 }; 
 const handler = NextAuth(authOptions); export { handler as GET, handler as POST };
--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,8 +1,13 @@
-'use client'; 
-import { useRouter } from 'next/navigation'; import { signIn } from 'next-auth/react'; import { useFormState } from '../hooks/useFormState'; import { AuthForm } from '../components/AuthForm';
-export default function LoginPage() { 
-    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const router = useRouter(); return <AuthForm 
-        title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} 
-        handleSubmit={e => {e.preventDefault(); signIn('credentials', { ...data, redirect: false }).then(result => { if (!result?.error) router.push('/dashboard') })}}
-    />;
+'use client';
+import { useRouter } from 'next/navigation'; import { FormEvent, useState } from 'react'; import { signIn } from 'next-auth/react'; import { useFormState } from '../hooks/useFormState';
+import { AuthForm } from '../components/AuthForm'; import ErrorComponent from '../components/ErrorComponent';
+export default function loginPage() { 
+    const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const [error, setError] = useState<string | null>(null); const router = useRouter();
+    const loginUser = async (e: FormEvent) => {
+        e.preventDefault(); const result = await signIn('credentials', { ...data, redirect: false }); if (result?.error) { setError(result.error); } else { router.push('/dashboard'); }
+    }
+    return <>
+            {error && <ErrorComponent message={error} onClose={() => setError(null)} />}
+            <AuthForm title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} handleSubmit={loginUser} />
+        </>;
 }
