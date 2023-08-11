98f4c100fc3e08c62ceb53cc95c8779d5ffd78f2
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-11 17:08:51 +0200

    "unimportant"

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -3,9 +3,8 @@ import { useRouter } from 'next/navigation'; import { FormEvent, useState } from
 import { AuthForm } from '../components/AuthForm'; import ErrorComponent from '../components/ErrorComponent';
 export default function loginPage() { 
     const [data, handleChange] = useFormState({ name: '', email: '', password: ''}); const [error, setError] = useState<string | null>(null); const router = useRouter();
-    const loginUser = async (e: FormEvent) => {
-        e.preventDefault(); const result = await signIn('credentials', { ...data, redirect: false }); if (result?.error) { setError(result.error); } else { router.push('/dashboard'); }
-    }
+    const loginUser = async (e: FormEvent) => 
+        { e.preventDefault(); const result = await signIn('credentials', { ...data, redirect: false }); if (result?.error) { setError(result.error); } else { router.push('/dashboard'); } }
     return <>
             {error && <ErrorComponent message={error} onClose={() => setError(null)} />}
             <AuthForm title="Sign in to your account" buttonText="Sign in" data={data} handleChange={handleChange} handleSubmit={loginUser} />
