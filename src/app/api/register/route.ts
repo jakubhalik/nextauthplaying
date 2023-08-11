54b4f879ae63fc07d22eba6a14c2f29060483f93
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-11 13:21:42 +0200

    "dynamicthrowingnameoremailintheregisterpopuperror"

--- src/app/api/register/route.ts
+++ src/app/api/register/route.ts
@@ -1,9 +1,12 @@
-import bcrypt from 'bcrypt'; import { PrismaClient } from '@prisma/client'; import { NextRequest, NextResponse } from 'next/server';
+import bcrypt from 'bcrypt'; import { PrismaClient, User } from '@prisma/client'; import { NextRequest, NextResponse } from 'next/server';
 const prisma = new PrismaClient();
 export async function POST(request: NextRequest) {
     const { name, email, password } = await request.json();
     if (!name || !email || !password) return new NextResponse('Missing name, email, or password', { status: 400 });
-    const existingUser = await prisma.user.findFirst({ where: { OR: [ { name }, { email } ] } });
-    if (existingUser) return NextResponse.json({ error: true, message: `${existingUser.email === email ? 'Email' : 'Name'} has already been registered`}, { status: 400 });
+    const existingUser: User | null = await prisma.user.findFirst({ where: { OR: [ { name }, { email } ] } });
+    if (existingUser) {
+        const field = existingUser.email === email ? 'Email' : 'Name'; const fieldValue = existingUser[field.toLowerCase() as keyof User];
+        return NextResponse.json({ error: true, message: `${field} "${fieldValue}" has already been registered`}, { status: 400 });
+    }
     return NextResponse.json(await prisma.user.create({ data: { name, email, hashedPassword: await bcrypt.hash(password, 10) } }));
 }
