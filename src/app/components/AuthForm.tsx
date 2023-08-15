dcd0412d4d1b8cd259cd3900a97e9a1887ab6a93
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-15 16:20:58 +0200

    "verygood"

--- src/app/components/AuthForm.tsx
+++ src/app/components/AuthForm.tsx
@@ -9,14 +9,14 @@ export const AuthForm: React.FC<AuthFormProps> = ({ title, buttonText, actionCal
     const [data, handleChange] = useFormState({ name: '', email: '', password: '' }); const [error, setError] = useState<string | null>(null); const router = useRouter();
     const handleSubmit = async (e: FormEvent<HTMLFormElement>) => {
         e.preventDefault(); 
-        try { const result = await actionCallback(data); if (result?.error) { setError(result.error); } else { router.push(push); } } catch (err) { setError(err.message || 'An error occurred.'); }
+        try { const result = await actionCallback(data); if (result?.error) { setError(result.error); } else { router.push(push); } } catch (err) { setError(err instanceof Error ? err.message : 'An error occurred'); }
     };
     return <>
         {error && <ErrorComponent message={error} onClose={() => setError(null)} />}
         <div className="flex justify-center py-24 3xl:py-56"><div className="border rounded-md p-4 sm:p-14">
             <h2 className="text-center text-2xl sm:text-3xl font-bold text-gray-900 mb-6">{title}</h2>
             <form onSubmit={handleSubmit}>
-                {Object.keys(data).map(key => <Input key={key} name={key} value={data[key]} onChange={handleChange} />)}
+                {Object.keys(data).map(key => { const inputKey = key as keyof typeof data; return <Input key={inputKey} name={inputKey} value={data[inputKey]} onChange={handleChange} />; })}
                 <div className="mt-4">
                     <button type="submit" 
                     className="flex w-full justify-center rounded-md bg-sky-500 p-2 text-white text-xl sm:text-2xl font-bold border-0 shadow-sm hover:bg-sky-400 active:bg-sky-300">
--- src/app/components/ErrorComponent.tsx
+++ src/app/components/ErrorComponent.tsx
@@ -1,6 +1,6 @@
 type ErrorProps = { message: string, onClose?: () => void }
 const ErrorComponent: React.FC<ErrorProps> = ({ message, onClose }) => 
-    { return <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 text-2xl rounded fixed w-full text-center" role="alert">
+    { return <div className="bg-red-100 border border-red-400 text-red-700 px-8 sm:px-4 py-3 text-lg sm:text-2xl rounded fixed w-full text-center" role="alert">
         {message}
         <button onClick={onClose}>
                 <span className="absolute top-0 bottom-0 right-0 px-4 py-3">
