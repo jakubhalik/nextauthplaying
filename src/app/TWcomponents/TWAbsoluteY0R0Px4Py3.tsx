7e7e08e87fe4d648b039e624dec492004fdd992e
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-28 12:03:55 +0200

    "topcomponentedforreusability"

---
+++ src/app/TWcomponents/TWAbsoluteY0R0Px4Py3.tsx
@@ -0,0 +1,4 @@
+interface TWAbsoluteY0R0Px4Py3Props { children: React.ReactNode }
+const TWAbsoluteY0R0Px4Py3: React.FC<TWAbsoluteY0R0Px4Py3Props> = ({ children }) => 
+    <div className="absolute top-0 bottom-0 right-0 px-4 py-3">{children}</div>;
+export default TWAbsoluteY0R0Px4Py3;
---
+++ src/app/TWcomponents/TWBgred100Borderred400Textred700Px8sm4Py3Textlgsm2xlRoundedFixedWfullTextcenter.tsx
@@ -0,0 +1,4 @@
+interface TWBgred100Borderred400Textred700Px8sm4Py3Textlgsm2xlRoundedFixedWfullTextcenterProps { children: React.ReactNode }
+const TWBgred100Borderred400Textred700Px8sm4Py3Textlgsm2xlRoundedFixedWfullTextcenter: React.FC<TWBgred100Borderred400Textred700Px8sm4Py3Textlgsm2xlRoundedFixedWfullTextcenterProps> = ({ children }) => 
+    <div className="bg-red-100 border border-red-400 text-red-700 px-8 sm:px-4 py-3 text-lg sm:text-2xl rounded fixed w-full text-center">{children}</div>;
+export default TWBgred100Borderred400Textred700Px8sm4Py3Textlgsm2xlRoundedFixedWfullTextcenter;
