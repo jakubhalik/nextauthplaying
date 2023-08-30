a528e02164cbfe3f93eb361d5b7307a34519bba1
jakubhalik <94850655+jakubhalik@users.noreply.github.com>
2023-08-30 20:20:44 +0200

    "sessionarounddashboardtosayhitothespecificuser"

--- src/app/dashboard/page.tsx
+++ src/app/dashboard/page.tsx
@@ -1,1 +1,8 @@
-export default function DashboardPage() { return <div>dashboard</div>; }
+'use client';
+import { useSession, SessionProvider } from 'next-auth/react';
+function DashboardContent() {
+    const { data: session, status } = useSession();
+    if (status === 'loading') { return <div>Loading...</div>; } if (!session || !session.user) { return <div>You are not authenticated!</div>; }
+    return <div>Hi {session.user.name}! Welcome to your dashboard</div>;
+}
+export default function DashboardPage( pageProps: any ) { return <SessionProvider session={pageProps.session}><DashboardContent /></SessionProvider>; }
